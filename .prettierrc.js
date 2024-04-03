f02502f8e8d4d71af5f26d6abba60c90b40476a5
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-04-03 15:30:17 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    'actions',
+    'data',
+    'schemas',
+    'lib',
+    'hooks',
+    'component',
+  ],
+  importOrderSeparation: true,
+};
+
+module.exports = config;
