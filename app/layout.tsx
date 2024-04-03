d680fbae4fdbdb9aac88f6c77ac282aa703394e6
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-04-03 16:03:18 +0200

    Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import React from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Live | Live App",
-  description: "Private live application",
+  title: 'Live | Live App',
+  description: 'Private live application',
 };
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import { Button } from '@/components/ui/button';
+import { config } from '@/config';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl={config.CLERK_AFTER_SIGN_OUT_URL} />
+    </div>
+  );
 }
